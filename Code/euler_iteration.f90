c83596164e400380dee3bdc31524a73095fc789f
electric-mile <etpc2@cam.ac.uk>
2024-12-15 20:17:00 +0000

    4th order ????

--- Code/euler_iteration.f90
+++ Code/euler_iteration.f90
@@ -6,9 +6,11 @@ use smooth_stencil
 implicit none
 type(t_appvars), intent(in) :: av
 type(t_grid), intent(inout) :: g
-real, dimension(g%ni,g%nj-1) :: mass_i, flux_i
-real, dimension(g%ni-1,g%nj) :: mass_j, flux_j
+real, dimension(g%ni,g%nj-1) :: mass_i, flux_i, rovx_i, rovy_i, hstag_i, vx_i, vy_i, p_i
+real, dimension(g%ni-1,g%nj) :: mass_j, flux_j, rovx_j, rovy_j, hstag_j, vx_j, vy_j, p_j
 real, dimension(g%ni-1,g%nj-1) :: mach, v, t
+integer :: order = 4
+
 integer :: i, j, ni, nj
 real :: dx
 
@@ -18,61 +20,138 @@ if (ni <= 0 .or. nj <= 0) then
       write(6,*) 'Euler Iteration Error: Grid dimensions must be positive and non-zero.'
       stop
 end if
-
-dx = g%x(2,1) - g%x(1,1)
-
-mass_i(:,1:nj-1) = ((g%rovx(:,1:nj-1)+g%rovx(:,2:nj))*g%lx_i(:,1:nj-1) + &
+select case (order)
+case (1)
+      mass_i(:,1:nj-1) = ((g%rovx(:,1:nj-1)+g%rovx(:,2:nj))*g%lx_i(:,1:nj-1) + &
                     (g%rovy(:,1:nj-1)+g%rovy(:,2:nj))*g%ly_i(:,1:nj-1))/2
-mass_j(1:ni-1,:) = ((g%rovx(1:ni-1,:)+g%rovx(2:ni,:))*g%lx_j(1:ni-1,:) + &
+      mass_j(1:ni-1,:) = ((g%rovx(1:ni-1,:)+g%rovx(2:ni,:))*g%lx_j(1:ni-1,:) + &
                     (g%rovy(1:ni-1,:)+g%rovy(2:ni,:))*g%ly_j(1:ni-1,:))/2
-                  
-
-where(g%wall(1:ni-1,:) .and. g%wall(2:ni,:)) mass_j = 0 
-where(g%wall(:,1:nj-1) .and. g%wall(:,2:nj)) mass_i = 0 
-
-call sum_fluxes(av, mass_i, mass_j, g%area, g%ro_start, g%ro, g%dro)
-
-if (any(g%ro <= 0.0)) then
-      write(6,*) 'Euler Iteration Error: Non-positive density values detected.'
-      ! Print the indices and values where the error occurs
-      do i = 1, ni
-          do j = 1, nj
-              if (g%ro(i,j) <= 0.0) then
-                  write(6,*) 'Non-positive density at (', i, ',', j, '): ro =', g%ro(i,j)
-              end if
-          end do
+      where(g%wall(1:ni-1,:) .and. g%wall(2:ni,:)) mass_j = 0 
+      where(g%wall(:,1:nj-1) .and. g%wall(:,2:nj)) mass_i = 0 
+      call sum_fluxes(av, mass_i, mass_j, g%area, g%ro_start, g%ro, g%dro, g)
+      if (any(g%ro <= 0.0)) then
+            write(6,*) 'Euler Iteration Error: Non-positive density values detected.'
+            do i = 1, ni
+            do j = 1, nj
+                  if (g%ro(i,j) <= 0.0) then
+                        write(6,*) 'Non-positive density at (', i, ',', j, '): ro =', g%ro(i,j)
+                  end if
+            end do
+            end do
+            stop
+      end if
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j)*(g%hstag(:,j) + g%hstag(:,j+1))/2
       end do
-      stop
-  end if
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*(g%hstag(i,:) + g%hstag(i+1,:))/2
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%roe_start, g%roe, g%droe, g)
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j)*(g%vx(:,j) + g%vx(:,j+1))/2 + ((g%p(:,j)+g%p(:,j+1))/2)*(g%lx_i(:,j))
+      end do
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*(g%vx(i,:) + g%vx(i+1,:))/2 + ((g%p(i,:)+g%p(i+1,:))/2)*(g%lx_j(i,:))
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%rovx_start, g%rovx, g%drovx, g)
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j)*(g%vy(:,j) + g%vy(:,j+1))/2 + ((g%p(:,j)+g%p(:,j+1))/2)*(g%ly_i(:,j))
+      end do
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*(g%vy(i,:) + g%vy(i+1,:))/2 + ((g%p(i,:)+g%p(i+1,:))/2)*(g%ly_j(i,:))
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%rovy_start, g%rovy, g%drovy, g)
+case(2)
+      mass_i(:,1:nj-1) = ((g%rovx(:,1:nj-1)+g%rovx(:,2:nj))*g%lx_i(:,1:nj-1) + &
+                    (g%rovy(:,1:nj-1)+g%rovy(:,2:nj))*g%ly_i(:,1:nj-1))/2
+      mass_j(1:ni-1,:) = ((g%rovx(1:ni-1,:)+g%rovx(2:ni,:))*g%lx_j(1:ni-1,:) + &
+                    (g%rovy(1:ni-1,:)+g%rovy(2:ni,:))*g%ly_j(1:ni-1,:))/2
+      where(g%wall(1:ni-1,:) .and. g%wall(2:ni,:)) mass_j = 0 
+      where(g%wall(:,1:nj-1) .and. g%wall(:,2:nj)) mass_i = 0 
+      call sum_fluxes(av, mass_i, mass_j, g%area, g%ro_start, g%ro, g%dro, g)
+      if (any(g%ro <= 0.0)) then
+            write(6,*) 'Euler Iteration Error: Non-positive density values detected.'
+            do i = 1, ni
+            do j = 1, nj
+                  if (g%ro(i,j) <= 0.0) then
+                        write(6,*) 'Non-positive density at (', i, ',', j, '): ro =', g%ro(i,j)
+                  end if
+            end do
+            end do
+            stop
+      end if
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j)*(g%hstag(:,j) + g%hstag(:,j+1))/2
+      end do
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*(g%hstag(i,:) + g%hstag(i+1,:))/2
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%roe_start, g%roe, g%droe, g)
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j)*(g%vx(:,j) + g%vx(:,j+1))/2 + ((g%p(:,j)+g%p(:,j+1))/2)*(g%lx_i(:,j))
+      end do
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*(g%vx(i,:) + g%vx(i+1,:))/2 + ((g%p(i,:)+g%p(i+1,:))/2)*(g%lx_j(i,:))
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%rovx_start, g%rovx, g%drovx, g)
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j)*(g%vy(:,j) + g%vy(:,j+1))/2 + ((g%p(:,j)+g%p(:,j+1))/2)*(g%ly_i(:,j))
+      end do
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*(g%vy(i,:) + g%vy(i+1,:))/2 + ((g%p(i,:)+g%p(i+1,:))/2)*(g%ly_j(i,:))
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%rovy_start, g%rovy, g%drovy, g)
+case(4)
+      call fourth_order_accuracy(g, g%rovx, rovx_i, rovx_j)
+      call fourth_order_accuracy(g, g%rovy, rovy_i, rovy_j)
+      mass_i(:,1:nj-1) = rovx_i(:,1:nj-1)*g%lx_i(:,1:nj-1) + &
+                    rovy_i(:,1:nj-1)*g%ly_i(:,1:nj-1)
+      mass_j(1:ni-1,:) = rovx_j(1:ni-1,:)*g%lx_j(1:ni-1,:) + &
+                    rovy_j(1:ni-1,:)*g%ly_j(1:ni-1,:)
+      where(g%wall(1:ni-1,:) .and. g%wall(2:ni,:)) mass_j = 0 
+      where(g%wall(:,1:nj-1) .and. g%wall(:,2:nj)) mass_i = 0 
+      call sum_fluxes(av, mass_i, mass_j, g%area, g%ro_start, g%ro, g%dro, g)
+      if (any(g%ro <= 0.0)) then
+            write(6,*) 'Euler Iteration Error: Non-positive density values detected.'
+            do i = 1, ni
+            do j = 1, nj
+                  if (g%ro(i,j) <= 0.0) then
+                        write(6,*) 'Non-positive density at (', i, ',', j, '): ro =', g%ro(i,j)
+                  end if
+            end do
+            end do
+            stop
+      end if
 
-do j = 1, nj-1
-      flux_i(:,j) = mass_i(:,j)*(g%hstag(:,j) + g%hstag(:,j+1))/2
-end do
-do i = 1, ni-1
-      flux_j(i,:) = mass_j(i,:)*(g%hstag(i,:) + g%hstag(i+1,:))/2
-end do
-!write(6,*) 'roe = ', g%roe(1,1), 'before ', av%nstep
-call sum_fluxes(av, flux_i, flux_j, g%area, g%roe_start, g%roe, g%droe) ! roe changes here I think @11976
-!write(6,*) 'roe = ', g%roe(1,1), 'after ', av%nstep
+      call fourth_order_accuracy(g, g%hstag, hstag_i, hstag_j)
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j) * hstag_i(:,j)
+      end do
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*hstag_j(i,:)
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%roe_start, g%roe, g%droe, g)
 
-do j = 1, nj-1
-      flux_i(:,j) = mass_i(:,j)*(g%vx(:,j) + g%vx(:,j+1))/2 + ((g%p(:,j)+g%p(:,j+1))/2)*(g%lx_i(:,j))
-end do
-do i = 1, ni-1
-      flux_j(i,:) = mass_j(i,:)*(g%vx(i,:) + g%vx(i+1,:))/2 + ((g%p(i,:)+g%p(i+1,:))/2)*(g%lx_j(i,:))
-end do
+      call fourth_order_accuracy(g, g%vx, vx_i, vx_j)
+      call fourth_order_accuracy(g, g%p, p_i, p_j)
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j)*(vx_i(:,j)) + (p_i(:,j))*(g%lx_i(:,j))
+      end do
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*(vx_j(i,:)) + (p_j(i,:))*(g%lx_j(i,:))
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%rovx_start, g%rovx, g%drovx, g)
 
-call sum_fluxes(av, flux_i, flux_j, g%area, g%rovx_start, g%rovx, g%drovx)
-
-do j = 1, nj-1
-      flux_i(:,j) = mass_i(:,j)*(g%vy(:,j) + g%vy(:,j+1))/2 + ((g%p(:,j)+g%p(:,j+1))/2)*(g%ly_i(:,j))
-end do
-do i = 1, ni-1
-      flux_j(i,:) = mass_j(i,:)*(g%vy(i,:) + g%vy(i+1,:))/2 + ((g%p(i,:)+g%p(i+1,:))/2)*(g%ly_j(i,:))
-end do
-
-call sum_fluxes(av, flux_i, flux_j, g%area, g%rovy_start, g%rovy, g%drovy)
-
+      call fourth_order_accuracy(g, g%vy, vy_i, vy_j)
+      call fourth_order_accuracy(g, g%p, p_i, p_j)
+      do j = 1, nj-1
+            flux_i(:,j) = mass_i(:,j)*(vy_i(:,j)) + (p_i(:,j))*(g%ly_i(:,j))
+      end do
+      do i = 1, ni-1
+            flux_j(i,:) = mass_j(i,:)*(vy_j(i,:)) + (p_j(i,:))*(g%ly_j(i,:))
+      end do
+      call sum_fluxes(av, flux_i, flux_j, g%area, g%rovy_start, g%rovy, g%drovy, g)
+end select
 call smooth_array(av,g%ro, g%corr_ro)
 call smooth_array(av,g%roe, g%corr_roe)
 call smooth_array(av,g%rovx, g%corr_rovx)
@@ -85,6 +164,5 @@ mach = v / ((av%gam * av%rgas * t)**0.5)
 write(6,*) 'Euler Iteration: Mach Number'
 write(6,*) 'roe =', g%roe(2,2),', ro =', g%ro(2,2), 'at ', av%nstep ! roe changes first
 write(6,*) 'mach =', mach(2,2),', velocity =', v(2,2),', tstat =', t(2,2), 'at ', av%nstep ! tstat changes first
-
 end subroutine euler_iteration
 
--- Code/flux_stencil.f90
+++ Code/flux_stencil.f90
@@ -1,17 +1,18 @@
       module flux_stencil
-
-      contains
-
-      subroutine sum_fluxes(av,flux_i,flux_j,area,start,prop,dcell)
-
       use types
       implicit none
+      contains
+
+
+      subroutine sum_fluxes(av,flux_i,flux_j,area,start,prop,dcell, g)
+
       type(t_appvars), intent(in) :: av
       
       real, intent(inout) :: flux_i(:,:), flux_j(:,:), area(:,:)
       real, intent(inout) :: prop(:,:)
       real, intent(inout) :: start(:,:)
       real, intent(inout) :: dcell(:,:)
+      type(t_grid), intent(inout) :: g
       real, dimension(size(prop,1),size(prop,2)) :: dnode
       real, dimension(size(dcell,1),size(dcell,2)) :: dcell_temp
       integer :: ni, nj, i, j
@@ -24,26 +25,32 @@
       end if
       dcell_temp = dcell
 
-      dcell(1:ni-1,1:nj-1) = (flux_i(1:ni-1,:) - flux_i(2:ni,:) + flux_j(:,1:nj-1) - flux_j(:,2:nj)) * (av%dt/area) 
 
+      dcell(1:ni-1,1:nj-1) = (flux_i(1:ni-1,:) - flux_i(2:ni,:) + flux_j(:,1:nj-1) - flux_j(:,2:nj)) * (av%dt/area)
+
+      ! call compute_dcell_cubic(dcell, flux_i, flux_j, area, av%dt, ni, nj, g)
 
       dcell = (1.0 + av%facsec) * dcell - av%facsec * dcell_temp
 
       dnode(2:ni-1,2:nj-1) = (dcell(1:ni-2, 1:nj-2) + dcell(2:ni-1,2:nj-1) + dcell(2:ni-1,1:nj-2) + dcell(1:ni-2, 2:nj-1))/4
-      do j = 2, nj-1
-            dnode(1, j) = 0.5 * (dcell(1, j) + dcell(1, j-1))
-            dnode(ni, j) = 0.5 * (dcell(ni-1, j) + dcell(ni-1, j-1))
-      end do
-      do i = 2, ni-1
-            dnode(i, 1) = 0.5 * (dcell(i, 1) + dcell(i-1, 1))
-            dnode(i, nj) = 0.5 * (dcell(i, nj-1) + dcell(i-1, nj-1))
-      end do
+      !      do j = 2, nj
+      !            dnode(1, j) = 0.5 * (dcell(1, j) + dcell(1, j-1))
+      !            dnode(ni, j) = 0.5 * (dcell(ni-1, j) + dcell(ni-1, j-1))
+      !      end do
+      !      do i = 2, ni
+      !            dnode(i, 1) = 0.5 * (dcell(i, 1) + dcell(i-1, 1))
+      !            dnode(i, nj) = 0.5 * (dcell(i, nj-1) + dcell(i-1, nj-1))
+      !      end do
+
+      dnode(2:ni-1,[1,nj]) = (dcell(1:ni-2, [1,nj-1]) + dcell(2:ni-1,[1,nj-1]))/2
+      dnode([1,ni],2:nj-1) = (dcell([1,ni-1], 1:nj-2) + dcell([1,ni-1], 2:nj-1))/2
 
       dnode(1, 1) = dcell(1, 1)
       dnode(1, nj) = dcell(1, nj-1)
       dnode(ni, 1) = dcell(ni-1, 1)
       dnode(ni, nj) = dcell(ni-1, nj-1)
       prop = start + dnode
+      !write(6,*) 'prop', prop
 
       dcell(1:ni-1,1:nj-1) = (flux_i(1:ni-1,:) - flux_i(2:ni,:) + flux_j(:,1:nj-1) - flux_j(:,2:nj))*(av%dt/area)
 
@@ -52,4 +59,43 @@
             !stop
       end if
       end subroutine sum_fluxes
+      
+      subroutine fourth_order_accuracy(g, prop, prop_i, prop_j)
+            real, intent(in) :: prop(:,:)
+            type(t_grid), intent(in) :: g
+            real, intent(out) :: prop_i(:,:), prop_j(:,:)
+            integer :: ni, nj, i, j
+            real :: a,b,c,d
+
+            ni  = size(prop,1)
+            nj = size(prop,2)
+
+            do j = 1, nj
+                  prop_i(1,j) = 0.5*(3*prop(1,j) - 4*prop(2,j) + prop(3,j))
+                  prop_i(2,j) = (-prop(1,j) + 6*prop(2,j) - 3*prop(3,j) - 2*prop(4,j))/8
+                  do i = 3, ni - 2
+                        a = prop(i-2, j)
+                        b = prop(i-1, j)
+                        c = prop(i+1, j)
+                        d = prop(i+2, j)
+                        prop_i(i,j) = (-a + 7*b + 7*c - d)/12
+                  end do
+                  prop_i(ni-1,j) = (-prop(ni-3,j) + 6*prop(ni-2,j) + 6*prop(ni-2,j) - 3*prop(ni-1,j) + prop(ni,j))/8
+                  prop_i(ni,j) = 0.5*(3*prop(ni,j) - 4*prop(ni-1,j) + prop(ni-2,j))
+            end do
+            do i = 1, ni
+                  prop_j(i,1) = 0.5*(3*prop(i,1) - 4*prop(i,2) + prop(i,3))
+                  prop_j(i,2) = (-prop(i,1) + 6*prop(i,2) - 3*prop(i,3) - 2*prop(i,4))/8
+                  do j = 3, nj - 2
+                        a = prop(i, j - 2)
+                        b = prop(i, j - 1)
+                        c = prop(i, j + 1)
+                        d = prop(i, j + 2)
+                        prop_j(i, j) = (-a + 7.0 * b + 7.0 * c - d) / 12.0
+                  end do
+                  prop_j(i, nj - 1) = (-prop(i, nj - 3) + 6.0 * prop(i, nj - 2) - 3.0 * prop(i, nj - 1) + prop(i, nj)) / 8.0
+                  prop_j(i, nj) = 0.5 * (3.0 * prop(i, nj) - 4.0 * prop(i, nj - 1) + prop(i, nj - 2))
+            end do
+      end subroutine fourth_order_accuracy
+
       end module flux_stencil
